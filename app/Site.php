b23252e36e9c18b74b53a60acb5cab00c4ab8055
pmoragas-bobo <pmoragas@bobochoses.com>
2018-06-15 13:12:07 +0200

    migrate, sitesController, ruta resources

--- app/Site.php
+++ app/Site.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Site extends Model
 {
-    protected $table = 'Sites';
+    protected $table = 'sites';
 
-    protected $primaryKey = 'Id';
+    protected $primaryKey = 'id';
 
-    protected $fillable = array('Url','Rank','IsMarfeelizable');
+    protected $fillable = array('url','rank','isMarfeelizable');
 
     protected $hidden = ['created_at','updated_at'];
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::Resource('sites', 'SitesController')->only([
+    'index', 'store'
+]);
